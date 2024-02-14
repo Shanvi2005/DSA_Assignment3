de1ee8a56d9ccc83bca9d0733f183825a8b86440
Shanvi2005 <144417466+Shanvi2005@users.noreply.github.com>
2024-02-14 21:29:24 +0530

    Update A3Q5.java

--- A3Q5.java
+++ A3Q5.java
@@ -1,22 +1,21 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if (nums.length == 1) {
-            return;
-        }
-
-        int nonZeroIndex = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[nonZeroIndex] = nums[i];
-                nonZeroIndex++;
+        int c = 0;
+        int count = 0;
+        int n = nums.length;
+        for (int j = 0; j < nums.length - 1; j++) {
+            if (nums[j] == 0) {
+                count++;
             }
         }
-
-       
-         while(nonZeroIndex<nums.length){
-            nums[nonZeroIndex] = 0;
-            nonZeroIndex++;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[c] = nums[i];
+                c++;
+            }
+        }
+        for (int k = c; k < n; k++) { // k=c se start krna not a complex logic
+            nums[k] = 0;
         }
     }
 }
